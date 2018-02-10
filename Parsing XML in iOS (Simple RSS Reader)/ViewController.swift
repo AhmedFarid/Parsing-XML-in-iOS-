78875d51fa60ceced1abea91a45246a9500cb02f
Ahmed Farido <AhmedFarido@github.com>
2018-02-10 17:25:37 +0200

    Initial Commit

---
+++ Parsing XML in iOS (Simple RSS Reader)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parsing XML in iOS (Simple RSS Reader)
+//
+//  Created by FARIDO on 2/10/18.
+//  Copyright © 2018 FARIDO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
